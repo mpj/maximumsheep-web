8738e94696aac05d17c0bf3799d5afa07d8942b7
Mattias Johansson <mpj@mpj.me>
2019-06-12 16:11:04 +0200

    added channel-id endpoint

--- src/modules/twitch.js
+++ src/modules/twitch.js
@@ -70,6 +70,24 @@ function tokenSetFromResponseBody(body) {
   }
 }
 
+exports.getChannelId = function getChannelId(fetch, clientId, oAuthAccessToken) {
+  return fetch('https://api.twitch.tv/kraken/channel', {
+    credentials: 'include',  
+    headers: {
+      'Client-ID': clientId,
+      'Authorization': 'OAuth ' + oAuthAccessToken
+    }
+  })
+  .then(assertResponseOK)
+  .then(parseResponseJSON)
+  .then(idFromChannelResponseData)
+
+  function idFromChannelResponseData(responseData) {
+    return responseData._id
+  }
+}
+
+
 const assertResponseOK = response => {
   if (response.status !== 200) {
     throw new Error(

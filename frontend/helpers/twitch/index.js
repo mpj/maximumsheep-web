7198e9f15a075633182b94db53cc8f25c8f491bd
Mattias Johansson <mpj@mpj.me>
2019-06-19 11:20:08 +0200

    added ci scripts to npm and unit tested module

---
+++ frontend/helpers/twitch/index.js
@@ -0,0 +1,13 @@
+module.exports.getChannelId = async function getChannelId(fetch, origin, secret) {
+  const response = await fetch(origin + '/channel-id', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ secret })
+    })
+  if (response.status !== 200) {
+    throw new Error('Response status was not okay (secret was probably incorrect)')
+  }
+  return response.text()
+}
---
+++ frontend/helpers/twitch/sandbox.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ })
+const twitch = require('.')
+const fetch = require('node-fetch')
+
+const origins = {
+  dev: 'http://localhost:3001',
+  staging: 'https://maximumsheep-web-backend-staging.now.sh/',
+  production: 'https://maximumsheep-web-backend-production.now.sh/'
+}
+
+const getChannelId = twitch.getChannelId.bind(null, fetch, origins.staging, process.env.BRIDGE_SECRET_STAGING)
+
+;(async function() {
+  const channelId = await getChannelId()
+  // eslint-disable-next-line no-console 
+  console.log('got channel id', channelId)
+})()

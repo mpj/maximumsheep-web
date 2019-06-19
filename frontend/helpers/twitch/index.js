bfb585a931a6720280ff124c9ebd6510c31bbbc0
Mattias Johansson <mpj@mpj.me>
2019-06-19 11:22:47 +0200

    Prettirr

--- frontend/helpers/twitch/index.js
+++ frontend/helpers/twitch/index.js
@@ -1,13 +1,19 @@
-module.exports.getChannelId = async function getChannelId(fetch, origin, secret) {
-  const response = await fetch(origin + '/channel-id', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ secret })
-    })
+module.exports.getChannelId = async function getChannelId(
+  fetch,
+  origin,
+  secret
+) {
+  const response = await fetch(origin + "/channel-id", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ secret })
+  });
   if (response.status !== 200) {
-    throw new Error('Response status was not okay (secret was probably incorrect)')
+    throw new Error(
+      "Response status was not okay (secret was probably incorrect)"
+    );
   }
-  return response.text()
-}
+  return response.text();
+};

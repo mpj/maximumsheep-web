1b240bdd1a0dece428089e6877da1e68e0bc75a0
Mattias Johansson <mpj@mpj.me>
2019-10-18 15:33:26 +0200

    make now use dotenv

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,12 @@
+require("dotenv").config()
+const webpack = require('webpack')
+
 module.exports = {
-  target: "serverless"
+  target: "serverless",
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env)
+    )
+    return config
+  }
 }
